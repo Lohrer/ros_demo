7ecb7d85d5ba608b2dcdfc720fa56d7db3ad8e1e
Lincoln Lorenz <llorenz@dataspeedinc.com>
2020-03-17 11:51:12 -0400

    Finish person_at_door_detector

--- people_counter/src/PersonAtDoorDetector.cpp
+++ people_counter/src/PersonAtDoorDetector.cpp
@@ -2,16 +2,88 @@
 
 namespace people_counter
 {
-  PersonAtDoorDetector::PersonAtDoorDetector(ros::NodeHandle& n, ros::NodeHandle& pn) {
+  PersonAtDoorDetector::PersonAtDoorDetector(ros::NodeHandle& n, ros::NodeHandle& pn) : it_(n) {
     pub_person_at_door_ = n.advertise<std_msgs::Bool>("person_at_door", 10);
-    sub_poses_ = n.subscribe("poses", 10, &PersonAtDoorDetector::recvPoses, this);
-    // TODO: add parameter that tells us dimensions and location of the door
+    sub_poses_ = n.subscribe("people", 10, &PersonAtDoorDetector::recvPoses, this);
+    sub_image_ = it_.subscribe("image", 1, &PersonAtDoorDetector::recvImage, this);
+    pub_image_ = it_.advertise("door_image", 1);
+    srv_.setCallback(boost::bind(&PersonAtDoorDetector::reconfig, this, _1, _2));
   }
 
-// TODO: make this use the actual poses message!!
-void PersonAtDoorDetector::recvPoses(const std_msgs::Bool::ConstPtr & msg) {
-  // TODO: filter poses that are near the door
+void PersonAtDoorDetector::recvPoses(const openpose_ros_msgs::OpenPoseHumanList::ConstPtr & msg) {
+  std_msgs::Bool out;
+  out.data = false;
+  // TODO: check if a person is inside the door
+  for (const auto& person : msg->human_list) {
+    const auto& box = person.body_bounding_box;
+     if (box.x > cfg_.x && box.y > cfg_.y && box.width < cfg_.width && box.height < cfg_.height) {
+       out.data = true;
+       break;
+     }
+  }
+  // save the person list so we can draw them on our image?
+  people_ = msg->human_list;
+  pub_person_at_door_.publish(out);
 }
 
+static void drawBox(cv_bridge::CvImagePtr cv_ptr, const float x1, const float y1, const float x2, const float y2, const cv::Scalar& color) {
+    // top
+    cv::line(cv_ptr->image, cv::Point(x1, y1), cv::Point(x2, y1), color, 2);
+    // right
+    cv::line(cv_ptr->image, cv::Point(x2, y1), cv::Point(x2, y2), color, 2);
+    // left
+    cv::line(cv_ptr->image, cv::Point(x1, y1), cv::Point(x1, y2), color, 2);
+    // bottom
+    cv::line(cv_ptr->image, cv::Point(x1, y2), cv::Point(x2, y2), color, 2);
+}
+
+void PersonAtDoorDetector::recvImage(const sensor_msgs::ImageConstPtr& msg)
+{
+  cv_bridge::CvImagePtr cv_ptr;
+  try
+  {
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
+
+  // draw the outline of the door on the image, so we can visualize the configuration
+  if (cv_ptr->image.rows == 960 && cv_ptr->image.cols == 1280) {
+    int x1 = cfg_.x;
+    int y1 = cfg_.y;
+    int x2 = cfg_.x + cfg_.width;
+    int y2 = cfg_.y + cfg_.height;
+    const cv::Scalar GREEN(0, 255, 0);
+    drawBox(cv_ptr, x1, y1, x2, y2, GREEN);
+  } else {
+    ROS_WARN_ONCE("image size is %ux%u, expected 1280x960", cv_ptr->image.rows, cv_ptr->image.cols);
+  }
+
+  // draw the people detected
+  const cv::Scalar RED(0, 0, 255);
+  for (const auto& person : people_) {
+    const auto& box = person.body_bounding_box;
+    drawBox(cv_ptr, box.x, box.y, box.x + box.width, box.y + box.height, RED);
+  }
+
+  // output modified image
+  pub_image_.publish(cv_ptr->toImageMsg());
+
+}
+
+void PersonAtDoorDetector::reconfig(PersonAtDoorDetectorConfig& config, uint32_t level) {
+  int right = config.x + config.width;
+  int bottom = config.y + config.height;
+  if (right >= 1280) {
+    config.width = 1280 - 1 - config.x;
+  }
+  if (bottom >= 960) {
+    config.height = 960 - 1 - config.y;
+  }
+  cfg_ = config;
+}
 
 }
--- people_counter/src/PersonAtDoorDetector.hpp
+++ people_counter/src/PersonAtDoorDetector.hpp
@@ -1,9 +1,22 @@
 #pragma once
 
 #include <ros/ros.h>
+#include "PersonAtDoorDetector.hpp"
+
+// messages
 #include <std_msgs/Bool.h>
 #include <std_msgs/Int16.h>
-#include "PersonAtDoorDetector.hpp"
+#include <openpose_ros_msgs/OpenPoseHumanList.h>
+
+// image handling
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+
+// dynamic reconfigure
+#include <dynamic_reconfigure/server.h>
+#include <people_counter/PersonAtDoorDetectorConfig.h>
 
 namespace people_counter
 {
@@ -13,10 +26,24 @@ namespace people_counter
       PersonAtDoorDetector(ros::NodeHandle& n, ros::NodeHandle& pn);
 
     private:
-      void recvPoses(const std_msgs::Bool::ConstPtr & msg);
+      void recvPoses(const  openpose_ros_msgs::OpenPoseHumanList::ConstPtr & msg);
+      void recvImage(const sensor_msgs::ImageConstPtr& msg);
+      void reconfig(PersonAtDoorDetectorConfig& config, uint32_t level);
 
       // publishers and subscribers
       ros::Publisher pub_person_at_door_;
       ros::Subscriber sub_poses_;
+
+      // image handling
+      image_transport::ImageTransport it_;
+      image_transport::Subscriber sub_image_;
+      image_transport::Publisher pub_image_;
+
+      // saved people, for visualization
+      std::vector<openpose_ros_msgs::OpenPoseHuman> people_;
+
+      // dynamic reconfigure
+      dynamic_reconfigure::Server<PersonAtDoorDetectorConfig> srv_;
+      PersonAtDoorDetectorConfig cfg_;
   };
 }
