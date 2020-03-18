88fe0ff4763d990ee4b29e3bfc87baad26eafdd0
Lincoln Lorenz <llorenz@dataspeedinc.com>
2020-03-17 22:54:35 -0400

    End of camera/people-at-door-detector demo

--- people_counter/src/PersonAtDoorDetector.cpp
+++ people_counter/src/PersonAtDoorDetector.cpp
@@ -3,31 +3,22 @@
 namespace people_counter
 {
   PersonAtDoorDetector::PersonAtDoorDetector(ros::NodeHandle& n, ros::NodeHandle& pn) : it_(n) {
-    // TODO: add subscribers and publishers
-    /*
+    // add subscribers and publishers
     pub_person_at_door_ = n.advertise<std_msgs::Bool>("person_at_door", 10);
     sub_poses_ = n.subscribe("people", 10, &PersonAtDoorDetector::recvPoses, this);
-    */
-    // TODO: add debugging
-    /*
+    // add debugging
     sub_image_ = it_.subscribe("image", 1, &PersonAtDoorDetector::recvImage, this);
     pub_image_ = it_.advertise("door_image", 1);
-    */
-    // TODO: add dynamic reconfigure
-    /*
+    // add dynamic reconfigure
     srv_.setCallback(boost::bind(&PersonAtDoorDetector::reconfig, this, _1, _2));
-    */
   }
 
 void PersonAtDoorDetector::recvPoses(const openpose_ros_msgs::OpenPoseHumanList::ConstPtr & msg) {
   std_msgs::Bool out;
   out.data = false;
-  // TODO: save the person list so we can draw them on our image?
-  /*
+  // save the person list so we can draw them on our image?
   people_ = msg->human_list;
-  */
-  // TODO: check if a person is inside the door
-  /*
+  // check if a person is inside the door
   for (const auto& person : msg->human_list) {
     const auto& box = person.body_bounding_box;
      if (box.x > cfg_.x && box.y > cfg_.y && box.width < cfg_.width && box.height < cfg_.height) {
@@ -35,7 +26,6 @@ void PersonAtDoorDetector::recvPoses(const openpose_ros_msgs::OpenPoseHumanList:
        break;
      }
   }
-  */
   pub_person_at_door_.publish(out);
 }
 
@@ -63,37 +53,36 @@ void PersonAtDoorDetector::recvImage(const sensor_msgs::ImageConstPtr& msg)
     return;
   }
 
-  // TODO: draw the people detected
-  /*
+  // draw the people detected
   const cv::Scalar RED(0, 0, 255);
+  const cv::Scalar GREEN(0, 255, 0);
   for (const auto& person : people_) {
     const auto& box = person.body_bounding_box;
-    drawBox(cv_ptr, box.x, box.y, box.x + box.width, box.y + box.height, RED);
+    if (box.x > cfg_.x && box.y > cfg_.y && box.width < cfg_.width && box.height < cfg_.height) {
+      drawBox(cv_ptr, box.x, box.y, box.x + box.width, box.y + box.height, GREEN);
+    } else {
+      drawBox(cv_ptr, box.x, box.y, box.x + box.width, box.y + box.height, RED);
+    }
   }
-  */
 
-  // TODO: draw the outline of the door on the image, so we can visualize the configuration
-  /*
+  // draw the outline of the door on the image, so we can visualize the configuration
   if (cv_ptr->image.rows == 960 && cv_ptr->image.cols == 1280) {
     int x1 = cfg_.x;
     int y1 = cfg_.y;
     int x2 = cfg_.x + cfg_.width;
     int y2 = cfg_.y + cfg_.height;
-    const cv::Scalar GREEN(0, 255, 0);
-    drawBox(cv_ptr, x1, y1, x2, y2, GREEN);
+    const cv::Scalar BLUE(255, 0, 0);
+    drawBox(cv_ptr, x1, y1, x2, y2, BLUE);
   } else {
     ROS_WARN_ONCE("image size is %ux%u, expected 1280x960", cv_ptr->image.rows, cv_ptr->image.cols);
   }
-  */
-  // TODO: output modified image
-  /*
+
+  // output modified image
   pub_image_.publish(cv_ptr->toImageMsg());
-  */
 
 }
 
-// TODO: add dynamic reconfigure
-/*
+// dynamic reconfigure
 void PersonAtDoorDetector::reconfig(PersonAtDoorDetectorConfig& config, uint32_t level) {
   int right = config.x + config.width;
   int bottom = config.y + config.height;
@@ -106,5 +95,4 @@ void PersonAtDoorDetector::reconfig(PersonAtDoorDetectorConfig& config, uint32_t
   }
   cfg_ = config;
 }
-*/
 }
